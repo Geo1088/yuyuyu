b3b6218d20f22d3fb97ee26fce8044f84b7c5d53
George <georgej1088@gmail.com>
2018-01-02 13:44:07 -0500

    Revise commands to be things

--- commands/help.js
+++ commands/help.js
@@ -5,10 +5,17 @@ module.exports = new Command(['help', 'h'], function (msg, args) {
 
   // If we got nothing, command list
   if (!args[0]) {
-    const commandList = this.commands.map(c => '`' + prefix + c.name + '`').join(', ')
+    let commandList = this.commands.map(c => c.name)
+
+    // Add other commands not included in this bot but used in others
+    commandList.push('addrole', 'removerole')
+    commandList.push('play', 'otherMusicBotCommands')
+
+    commandList = commandList.map(c => '`' + prefix + c + '`').join(', ')
+
     return msg.channel.createMessage(`**=== Help: Command List ===**
 You can use the following commands: ${commandList}
-Use \`${prefix}help [command]\` to get more info on that command!`)
+For full help information, see here: <https://geo1088.me/yuyuyu>`)
   }
 
   // We got a command, let's try using it
@@ -18,7 +25,4 @@ Use \`${prefix}help [command]\` to get more info on that command!`)
   // Rip, error
   msg.channel.createMessage(`**=== Help: Unknown Command ===**
 Make sure you spelled the command name right, and that this bot has it. Do \`${prefix}help\` with no arguments to see a list of commands.`)
-}, {
-  desc: 'Get a list of commands. Pass a command name as an argument to get information about that command.',
-  args: '[command]'
 })
--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,4 @@ const {Command} = require('yuuko')
 
 module.exports = new Command('ping', function (msg) {
   msg.channel.createMessage("I'm here.")
-}, {
-  desc: 'Pings the bot.',
-  args: ''
 })
